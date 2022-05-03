30eae6a8a564a31e33a1e8acf76072b006663be8
silianpan <liu.pan@silianpan.cn>
2022-05-03 17:32:01 +0800

    输入命令自动提示

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -128,3 +128,20 @@ cmp.setup {
     -- native_menu = false,
   },
 }
+
+-- Use buffer source for `/`.
+cmp.setup.cmdline("/", {
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
