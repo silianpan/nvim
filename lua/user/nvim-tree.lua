d67a35bb61e77925a4221b20dfd83dc5a74d074b
silianpan <liu.pan@silianpan.cn>
2022-05-05 11:22:00 +0800

    添加F，refresh命令

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -102,6 +102,7 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "F", cb = tree_cb "refresh" },
       },
     },
     number = false,
