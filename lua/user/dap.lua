440f435cf814f2acc08eb57d6bd72d2216d7dc2a
silianpan <liu.pan@silianpan.cn>
2022-08-12 09:53:46 +0800

    nvim-dap-ui中过时配置修改

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -19,26 +19,55 @@ dapui.setup {
     repl = "r",
     toggle = "t",
   },
-  sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
+  -- sidebar和tray过时
+  -- sidebar = {
+  --   -- You can change the order of elements in the sidebar
+  --   elements = {
+  --     -- Provide as ID strings or tables with "id" and "size" keys
+  --     {
+  --       id = "scopes",
+  --       size = 0.25, -- Can be float or integer > 1
+  --     },
+  --     { id = "breakpoints", size = 0.25 },
+  --     -- { id = "stacks", size = 0.25 },
+  --     -- { id = "watches", size = 00.25 },
+  --   },
+  --   size = 40,
+  --   position = "right", -- Can be "left", "right", "top", "bottom"
+  -- },
+  -- tray = {
+  --   elements = {},
+  --   -- elements = { "repl" },
+  --   -- size = 10,
+  --   -- position = "bottom", -- Can be "left", "right", "top", "bottom"
+  -- },
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+      -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.25 },
+        "breakpoints",
+        "stacks",
+        "watches",
       },
-      { id = "breakpoints", size = 0.25 },
-      -- { id = "stacks", size = 0.25 },
-      -- { id = "watches", size = 00.25 },
+      size = 40, -- 40 columns
+      position = "left",
     },
-    size = 40,
-    position = "right", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    elements = {},
-    -- elements = { "repl" },
-    -- size = 10,
-    -- position = "bottom", -- Can be "left", "right", "top", "bottom"
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
   },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
