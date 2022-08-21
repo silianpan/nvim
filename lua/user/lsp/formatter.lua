a7ab0539e22556f131664d30e5f29659e1f2ecc2
silianpan <1206284818@qq.com>
2022-08-22 07:49:10 +0800

    prettier格式化参数

--- lua/user/lsp/formatter.lua
+++ lua/user/lsp/formatter.lua
@@ -35,7 +35,27 @@ formatter.setup({
       function()
         return {
           exe = "prettier",
-          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
+          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote", "--config", "~/.prettierrc" },
+          stdin = true,
+        }
+      end,
+    },
+    vue = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote", "--config", "~/.prettierrc" },
+          stdin = true,
+        }
+      end,
+    },
+    typescript = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote", "--config", "~/.prettierrc" },
           stdin = true,
         }
       end,
