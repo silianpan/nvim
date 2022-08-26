c455a79ec58fd59cb8a91fc7e5c764087c09f881
silianpan <liu.pan@silianpan.cn>
2022-08-26 14:05:10 +0800

    feat: 添加Github主题

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -214,6 +214,32 @@ return packer.startup(function(use)
   -- TypeScript 增强
   use { "jose-elias-alvarez/nvim-lsp-ts-utils", requires = "nvim-lua/plenary.nvim" }
 
+  -- Github Theme
+  use {
+    'projekt0n/github-nvim-theme',
+    config = function()
+      -- Example config in Lua
+      require("github-theme").setup({
+        theme_style = "dark_colorblind", -- dark,dimmed,dark_default,dark_colorblind,light,light_default,light_colorblind
+        function_style = "italic",
+        sidebars = {"qf", "vista_kind", "terminal", "packer"},
+
+        -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+        colors = {hint = "orange", error = "#ff0000"},
+
+        -- Overwrite the highlight groups
+        overrides = function(c)
+          return {
+            htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+            DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+            -- this will remove the highlight groups
+            TSField = {},
+          }
+        end
+      })
+    end
+  }
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
