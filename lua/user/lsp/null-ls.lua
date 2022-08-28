afb5a62a3976450b57270365f9eb43c9193b1ea8
silianpan <1206284818@qq.com>
2022-08-28 12:09:03 +0800

    fix: Avoiding LSP formatting conflicts

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -10,7 +10,36 @@ local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 -- npm install --save-dev prettier prettier-plugin-solidity
+
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.formatting_sync({
+      filter = function(client)
+          -- apply whatever logic you want (in this example, we'll only use null-ls)
+          return client.name == "null-ls"
+      end,
+      bufnr = bufnr,
+  })
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- add to your shared on_attach callback
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+              lsp_formatting(bufnr)
+          end,
+      })
+  end
+end
+
 null_ls.setup {
+  on_attach = on_attach,
   debug = false,
   sources = {
     formatting.prettier.with {
