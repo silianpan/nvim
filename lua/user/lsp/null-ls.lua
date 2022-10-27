b5ae69273a868689b69714a00c15b5396b479758
silianpan <liu.pan@silianpan.cn>
2022-10-27 11:53:21 +0800

    nvim0.8版本兼容

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,12 +12,13 @@ local diagnostics = null_ls.builtins.diagnostics
 -- npm install --save-dev prettier prettier-plugin-solidity
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.formatting_sync({
+  vim.lsp.buf.format({
       filter = function(client)
           -- apply whatever logic you want (in this example, we'll only use null-ls)
           return client.name == "null-ls"
       end,
       bufnr = bufnr,
+      async = true,
   })
 end
 
