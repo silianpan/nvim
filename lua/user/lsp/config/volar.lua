d97bcc2fa1429ce91cf89cee23375bb10915ec47
silianpan <liu.pan@silianpan.cn>
2022-10-27 11:06:33 +0800

    nvim0.8版本兼容

--- lua/user/lsp/config/volar.lua
+++ lua/user/lsp/config/volar.lua
@@ -41,8 +41,8 @@ return {
       },
       on_attach = function(client, bufnr)
         -- 禁用格式化功能，交给专门插件插件处理
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
+        client.server_capabilities.document_formatting = false
+        client.server_capabilities.document_range_formatting = false
         client.server_capabilities.documentFormattingProvider = false -- 0.8 and later
         local function buf_set_keymap(...)
           vim.api.nvim_buf_set_keymap(bufnr, ...)
