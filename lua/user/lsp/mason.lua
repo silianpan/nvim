5f6f963ae3726e452c7e5d42cdf09c6739435910
silianpan <liu.pan@silianpan.cn>
2023-02-13 17:18:57 +0800

    fix: 更新版本

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -17,7 +17,7 @@ local servers = {
   "jsonls",
   "solc",
   "solidity",
-  "sumneko_lua",
+  "lua_ls",
   "tflint",
   "terraformls",
   "tsserver",
@@ -78,12 +78,12 @@ for _, server in pairs(servers) do
     opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
   end
 
-  if server == "sumneko_lua" then
+  if server == "lua_ls" then
     local l_status_ok, lua_dev = pcall(require, "lua-dev")
     if not l_status_ok then
       return
     end
-    -- local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+    -- local sumneko_opts = require "user.lsp.settings.lua_ls"
     -- opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
     -- opts = vim.tbl_deep_extend("force", require("lua-dev").setup(), opts)
     local luadev = lua_dev.setup {
@@ -95,7 +95,7 @@ for _, server in pairs(servers) do
         --   -- settings = opts.settings,
       },
     }
-    lspconfig.sumneko_lua.setup(luadev)
+    lspconfig.lua_ls.setup(luadev)
     goto continue
   end
 
--- lua/user/lsp/settings/tsserver.lua
+++ lua/user/lsp/settings/tsserver.lua
@@ -1,4 +1,13 @@
+local util = require "lspconfig.util"
+
 return {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  init_options = {
+    hostInfo = "neovim"
+  },
+  root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+  single_file_support = true,
   settings = {
     typescript = {
       inlayHints = {
