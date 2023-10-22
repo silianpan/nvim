7b47706cb43c737a835ba1328bd2935fcf272aec
silianpan <1206284818@qq.com>
2023-10-22 10:31:51 +0800

    indent-blankline升级之3.x版本

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,29 +1,29 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
   return
 end
 
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-  "help",
-  "startify",
-  "dashboard",
-  "packer",
-  "neogitstatus",
-  "NvimTree",
-  "Trouble",
-  "text"
-}
+-- vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+-- vim.g.indent_blankline_filetype_exclude = {
+--   "help",
+--   "startify",
+--   "dashboard",
+--   "packer",
+--   "neogitstatus",
+--   "NvimTree",
+--   "Trouble",
+--   "text"
+-- }
 vim.g.indentLine_enabled = 1
 -- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
+-- vim.g.indent_blankline_char = "▏"
 -- vim.g.indent_blankline_char = "▎"
 
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = false
 -- vim.g.indent_blankline_use_treesitter_scope = 1
-vim.g.indent_blankline_show_current_context = true
+-- vim.g.indent_blankline_show_current_context = true
 -- vim.g.indent_blankline_context_patterns = {
 --   "class",
 --   "return",
@@ -63,11 +63,30 @@ vim.g.indent_blankline_show_current_context = true
 indent_blankline.setup {
   -- show_end_of_line = true,
   -- space_char_blankline = " ",
-  show_current_context = true,
+  -- show_current_context = true,
   -- show_current_context_start = true,
   -- char_highlight_list = {
   --   "IndentBlanklineIndent1",
   --   "IndentBlanklineIndent2",
   --   "IndentBlanklineIndent3",
   -- },
+  exclude = {
+    buftypes = { "terminal", "nofile"},
+    filetypes = {
+      "help",
+      "startify",
+      "dashboard",
+      "packer",
+      "neogitstatus",
+      "NvimTree",
+      "Trouble",
+      "text"
+    },
+  },
+  indent = {
+    char = "▏"
+  },
+  scope = {
+    enabled = true,
+  },
 }
